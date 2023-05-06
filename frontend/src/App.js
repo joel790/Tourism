62c324c99e3bfbe955bc5595cdcec66a6357a6c1
joel790 <eyuelkassahun3@gmail.com>
2023-05-06 04:58:55 +0300

    frontend folder added

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div>
+      <h1>hello Guys this is the react initialization</h1>
+    <h4>you can start here</h4>
+    
+
+    </div>
+  );
+}
+
+export default App;
