54aa337b45bbbcc5fdf7bc992e5fe78ad050fa46
Bewketget <103607312+Bewketget@users.noreply.github.com>
2023-06-03 16:12:35 +0300

    routes for tourist's dashboard

---
+++ backend/routes/feedbackRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const feedbackController = require('../controllers/feedbackController');
+
+// Define routes for feedbacks
+router.post('/', feedbackController.createFeedback);
+router.get('/', feedbackController.getAllFeedbacks);
+router.get('/:id', feedbackController.getFeedbackById);
+router.put('/:id', feedbackController.updateFeedbackById);
+router.delete('/:id', feedbackController.deleteFeedbackById);
+
+module.exports = router;
