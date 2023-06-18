172ca9104c9c939800e5d0ec288a1d3fbd8e5e1b
joel790 <eyuelkassahun3@gmail.com>
2023-06-18 20:50:38 +0300

    working on hotel page of user

--- backend/routes/roomRoute.js
+++ backend/routes/roomRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { createRoom, getAllRooms, updateRoom, getRoom, deleteRoom } = require('../controllers/roomController');
+const { hotel } = require('../moddleWare/authMiddleware');
 const router = express.Router();
 router.get("/",  getAllRooms);
-router.post("/",createRoom);
-router.patch("/:id",updateRoom);
+router.post("/",hotel,createRoom);
+router.patch("/:id",hotel,updateRoom);
 router.get("/:id",getRoom);
-router.delete("/:id",deleteRoom);
+router.delete("/:id",hotel,deleteRoom);
 
 module.exports = router;
